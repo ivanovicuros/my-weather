9b9abdf0868609d43e3f683e0b49a406dea34498
Uros <urosivanovich@gmail.com>
2022-03-17 14:19:03 -0400

    redux, thunk setup

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { getForecast } from '../actions'
 
-const Weather = () => {
- 
+
+const Weather = (props) => {
+ const [city, setCity] = useState('')
+
+
+
+ const handleChange = e => {
+     setCity({
+         ...city,
+         [e.target.name]: e.target.value
+     })
+ }
+ const handleSubmit = e => {
+    e.preventDefault();
+    props.dispatch(getForecast(city))
+ }
     
-
     return (
         <>
         <h1>My Weather</h1>
--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import 'dotenv/config';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+
+import App from './App';
+import { reducer } from './reducer';
+
+const store = createStore(reducer, applyMiddleware(thunk))
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
