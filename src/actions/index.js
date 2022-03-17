45c1b58bc6ae495f98d04ed643703e9894535f04
Uros <urosivanovich@gmail.com>
2022-03-17 13:44:21 -0400

    env file, actions fixed

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,23 @@
+import axios from 'axios';
 const FETCH_START = 'FETCH_START';
 const FETCH_SUCCESS = 'FETCH_SUCCESS';
 const FETCH_FAIL = 'FETCH_FAIL';
+
+const url = process.env.BASE_URL
+const appId = process.env.APP_ID
+const apiKey = process.env.API_KEY
+
+
+export const getForecast = (city) => (dispatch) => {
+    dispatch(fetchStart())
+    axios.get(`${url}${city}${appId}${apiKey}`)
+    .then(res => {
+        dispatch(fetchSuccess(res))
+    })
+    .catch(err => {
+        dispatch(fetchFail(err))
+    })
+}
 
 export const fetchStart = () => {
     return ({type: FETCH_START});
